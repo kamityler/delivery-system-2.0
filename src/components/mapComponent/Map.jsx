04551832f739f388da8c6daff373daa8837b26b4
Danylo <bodnar.dp@gmail.com>
2024-10-16 19:53:12 +0300

    geocoding added

--- src/components/mapComponent/Map.jsx
+++ src/components/mapComponent/Map.jsx
@@ -1,20 +1,28 @@
 import React, { useRef, useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-control-geocoder';
+import 'leaflet-control-geocoder/dist/Control.Geocoder.css';
 import './Map.css'
 import { useMapUserLocation } from './../../hooks';
+import MapLoctionDefine from './geocoder';
 
 const Map = () => {
-    const mapRef = useRef(null);
     const {position} = useMapUserLocation();
+    return (
+        <MapContainer className='map-container' center={position} zoom={15}>
+            <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            <MapLoctionDefine/>
+        </MapContainer>
+    )
+    // const {position} = useMapUserLocation();
     
 
-    useEffect(()=>{
-        const map = mapRef.current?.leafletElement;
-        if (!map) return;
-        console.log(mapRef);
-    })
+    // useEffect(()=>{
+    //     const map = mapRef.current?.leafletElement;
+    //     if (!map) return;
+    //     console.log(mapRef);
+    // })
 
     // useEffect(() => {
     //     const L = window.L;
@@ -39,14 +47,14 @@ const Map = () => {
     //         .addTo(map);
     // }, []);
 
-    return (
-        <MapContainer className='map-container' center={position} zoom={15} ref={mapRef}>
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'/>
-            <Marker position={position}>
-                <Popup> {position.lat} , {position.lng} </Popup>
-            </Marker>
-        </MapContainer>
-    );
+    // return (
+    //     <MapContainer className='map-container' center={position} zoom={15} ref={mapRef}>
+    //         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'/>
+    //         <Marker position={position}>
+    //             <Popup> {position.lat} , {position.lng} </Popup>
+    //         </Marker>
+    //     </MapContainer>
+    // );
 }
 
 export default Map;
