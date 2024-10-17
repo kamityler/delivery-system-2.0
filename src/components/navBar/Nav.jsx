9c3f1e6eb9a4a65e4212952be89dc419ad8355ae
Danylo <bodnar.dp@gmail.com>
2024-10-17 23:31:52 +0300

    improving navs

--- src/components/navBar/Nav.jsx
+++ src/components/navBar/Nav.jsx
@@ -1,19 +1,18 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary" sticky="top">
       <Container>
-        <Navbar.Brand href="#home">Delivery System</Navbar.Brand>
+        <Navbar.Brand className="label" href="#home">Delivery System</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Couriers</Nav.Link>
             <Nav.Link href="#link">Map</Nav.Link>
-            <Nav.Link href="#link">Deliveries</Nav.Link>
+            <Nav.Link href="#deliveries">Deliveries</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
