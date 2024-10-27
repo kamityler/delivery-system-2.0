92ba485be9f1de819247b17135a68799072065a7
Danylo <bodnar.dp@gmail.com>
2024-10-27 23:39:44 +0200

    welcome page changes

--- src/page/WelcomePage/WelcomePage.jsx
+++ src/page/WelcomePage/WelcomePage.jsx
@@ -2,7 +2,6 @@ const WelcomePage = ({children}) => {
     return (
         <>
             <p>Welcome to ypur logisti system assist</p>
-            <a>login</a> <a>sign up</a>
             {children}
 
         </>
