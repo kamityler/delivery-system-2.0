ea05e6ed7344eec226d40de1ba7b7a29ac7c999f
Danylo <bodnar.dp@gmail.com>
2024-10-30 23:31:22 +0200

    developing login form

--- src/page/WelcomePage/LoginForm.jsx
+++ src/page/WelcomePage/LoginForm.jsx
@@ -1,8 +1,20 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+
 function LoginForm(){
     return (
-        <>
-        <p>this is login form</p>
-        </>
+        <Form>
+            <Form.Group>
+                <Form.Label controlId="formBasicEmail">Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter your email"/>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label controlId="formBasicPassword">Password</Form.Label>
+                <Form.Control type="password" placeholder="Enter your password"/>
+            </Form.Group>
+            <Button type="submit"> Enter </Button>
+        </Form>
     )
 }
 
