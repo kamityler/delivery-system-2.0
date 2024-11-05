3373878a7b349ce23d0187fdc798fdc41039ae59
Danylo <bodnar.dp@gmail.com>
2024-11-05 21:43:16 +0200

    sign up form developed

--- src/page/WelcomePage/SignupForm.jsx
+++ src/page/WelcomePage/SignupForm.jsx
@@ -1,8 +1,20 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 function SignupForm(){
     return (
-        <>
-        <p>this is signup form</p>
-        </>
+        <Form>
+            <h1 className='form-header'>Sign up!</h1>
+            <Form.Group >
+                <Form.Label controlId="formBasicEmail">Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter your email"/>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label controlId="formBasicPassword">Password</Form.Label>
+                <Form.Control type="password" placeholder="Enter your password"/>
+            </Form.Group>
+            <Button type="submit"> Enter </Button>
+        </Form>
     )
 }
 
