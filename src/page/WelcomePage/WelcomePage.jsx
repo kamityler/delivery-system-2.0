b265293bf8b617d272d020eb91c89d5d34141a1c
Danylo <bodnar.dp@gmail.com>
2024-11-05 22:07:27 +0200

    wp-sizes

--- src/page/WelcomePage/WelcomePage.jsx
+++ src/page/WelcomePage/WelcomePage.jsx
@@ -5,13 +5,13 @@ const WelcomePage = ({children}) => {
 
         <Container fluid className="wp-container">
             <Row className="h-100">
-                <Col xs={12} md={6} className="d-flex align-items-center justify-content-center left-side">
+                <Col xs={12} md={8} className="d-flex align-items-center justify-content-center left-side">
                     <div>
-                        <h1>Welcome to your perfect logistic assist!</h1>
-                        <p>We are glad to see you! Let's build the routes now!</p>
+                        <h1 className='header-wp'>Welcome to your perfect logistic assist!</h1>
+                        <p className='text-wp'>We are glad to see you! Let's build the routes now!</p>
                     </div>
                 </Col>
-                <Col xs={12} md={6} className="d-flex align-items-center justify-content-center right-side">
+                <Col xs={12} md={4} className="d-flex align-items-center justify-content-center right-side">
                     {children}
                 </Col>
             </Row>
